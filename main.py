ef0f18a450d2bec4c50cdef11f14b449ec7f5f4a
Gagan Malvi <malvi@aospa.co>
2021-02-11 19:33:48 +0530

    csak: Remove logo on screen
    * Takes up lots of space
    * Doesn't look good
    * Breaks compatibility on macOS
    
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ class GMInstaller(Gtk.Window):
         hbox = Gtk.Box(spacing=6)
         hbox.set_orientation(Gtk.Orientation.VERTICAL)
         self.add(hbox)
-        img = Gtk.Image.new_from_file('assets/logo.jpg')
-        hbox.pack_start(img, True, True, 0)
         self.aboutDevice = Gtk.Button(label='Check Connected Device')
         self.aboutDevice.connect("clicked", self.on_about_device)
         hbox.pack_start(self.aboutDevice, True, True, 0)
